16a8e5705ed1b53dcefefc3947c1e7e051954268
abhigithub2309 <61849731+abhigithub2309@users.noreply.github.com>
2023-07-20 15:49:33 +0530

    Add files via upload

---
+++ a.ps1
@@ -0,0 +1,13 @@
+echo "running powershell"
+
+#cd C:\Abhijittt
+
+$param = $args[0]
+
+#if ($param -eq 'DEV')
+#{ 
+	#write-host 'The parameter is $param' 
+#echo "$param"
+#}
+
+echo "platform value is $param"
